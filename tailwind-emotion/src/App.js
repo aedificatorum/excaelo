63aa06a90dfbc11b79c95b78b1dd3fab509a561f
Tim Addison <git@tjaddison.com>
2019-06-11 20:11:35 -0400

    Some examples with tailwind

--- tailwind-emotion/src/App.js
+++ tailwind-emotion/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
 import styled from '@emotion/styled';
 import tw from 'tailwind.macro';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
 
 const Header = styled.header`
 ${tw`bg-red-900 min-h-screen flex flex-col items-center justify-center text-xl text-white`};
@@ -14,10 +15,25 @@ function App() {
   return (
     <div>
       <Header>
-        THE HEADER
+        This requires styled + the tw macro
       </Header>
       <Main>
-        Yo
+        <p css={{
+          color: 'red'
+        }}>
+          This only requires the pragma + jsx import
+        </p>
+        <p css={css`
+          color: hotpink;
+        `}>
+          This requires the css import
+        </p>
+        <p css={tw`bg-red-300`}>
+          This works as you'd expect because it was referenced via the tw macro
+        </p>
+        <p className="bg-red-300">
+          And as you can see this one does not!
+        </p>
       </Main>
     </div>
   );
